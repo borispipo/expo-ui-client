24f966e5a33e22a02a5105c97845f19bb191151d
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-02-25 12:33:56 +0100

    Fix bug sur le script de génération de l'application cliente

--- bin/index.js
+++ bin/index.js
@@ -8,58 +8,58 @@
 'use strict';
 
 const { program } = require('commander');
-
+const path = require("path");
+const fs = require("fs");
 const packageObj = require("../package.json");
 const dir = path.resolve(__dirname,"..");
 const appJSON = require(path.resolve(dir,"app.json"));
 const version = packageObj.version;
 const packageName = packageObj.name;
-const path = require("path");
-const fs = require("fs");
-const {exec,thowError,copy,writeFile,createDirSync,FILE:{sanitizeFileName},JSONFileManager} = require("@fto-consult/node-utils");
-program
-  .name(packageName)
-  .description(`Utilitaire cli lié au framework ${packageName}`)
-  .version(version);
-  
-program.command('init')
-  .description(`crèe et initialise une application ${packageName}`)
-  .argument('<appName>', 'le nom de l\'application à initialiser')
-  .option('-r, --project-root [dir]', 'le project root de l\'application')
-  .action((appName, options) => {
-    const projectRoot = path.resolve(process.cwd(),appName);
-    console.log("création du répertoire de l'application "+appName);
-    createDirSync(projectRoot);
-    ["assets","src","App","babel.config.js","metro.config.js"].map((p)=>{
-        copy(path.resolve(dir,p),path.resolve(projectRoot,p));
-        const destGitIgnore = path.resolve(projectRoot,".gitignore");
+const {exec,thowError,copy,writeFile,createDirSync} = require("@fto-consult/node-utils");
+
+const script = process.argv[2];
+const appName = process.argv[3];
+
+if(!script || script !=="init"){
+    throw "Vous devez spécifier la tâche que vous souhaitez faire via l'un des scripts : "+["init"].join(",");
+}
+if(!appName || !appName.trim()){
+    throw "Vous devez spécifier le nom de l'application à initialiser"
+}
+
+const projectRoot = path.resolve(process.cwd(),appName);
+console.log("création du répertoire de l'application "+appName);
+createDirSync(projectRoot);
+["assets","src","App.js","babel.config.js","metro.config.js"].map((p)=>{
+    copy(path.resolve(dir,p),path.resolve(projectRoot,p));
+    const destGitIgnore = path.resolve(projectRoot,".gitignore");
+    try {
+        if(!fs.existsSync(destGitIgnore)){
+            fs.copyFileSync(path.resolve(dir,".gitignore"),destGitIgnore);
+        }
+    } catch{}
+    const destPackageJson = path.resolve(projectRoot,"package.json");
+    const destAppJSON = path.resolve(projectRoot,"app.json");
+    if(!fs.existsSync(destPackageJson)){
+        packageObj.name = appName;
+        packageObj.version = "1.0.0";
         try {
-            if(!fs.existsSync(destGitIgnore)){
-                fs.copyFileSync(path.resolve(dir,".gitignore"),destGitIgnore);
-            }
-        } catch{}
-        const destPackageJson = path.resolve(projectRoot,"package.json");
-        const destAppJSON = path.resolve(projectRoot,"app.json");
-        if(!fs.existsSync(destPackageJson)){
-            packageObj.name = appName;
-            packageObj.version = "1.0.0";
-            try {
-                fs.writeFileSync(destPackageJson,JSON.stringify(packageObj,null,2));
-            } catch{ }
-        }
-        if(!fs.existsSync(destAppJSON)){
-            appJSON.name = appName;
-            appJSON.slug = appName.trim().toLowerCase();
-            try {
-                fs.writeFileSync(destAppJSON,JSON.stringify(appJSON,null,2));
-            } catch{ }
-        }
-        if(fs.existsSync(destPackageJson)){
-            console.log("installation des packages....");
-            exec(`npm install`,{projectRoot}).finally(()=>{
-                console.log("application "+appName+" créée.");
-            });
-        }
-        
-    });
-  });
+            writeFile(destPackageJson,JSON.stringify(packageObj,null,2));
+        } catch{ }
+    }
+    if(!fs.existsSync(destAppJSON)){
+        appJSON.expo.name = appName;
+        appJSON.expo.slug = appJSON.expo.scheme = appName.trim().toLowerCase();
+        appJSON.expo.version = "1.0.0";
+        try {
+            writeFile(destAppJSON,JSON.stringify(appJSON,null,2));
+        } catch{ }
+    }
+    if(fs.existsSync(destPackageJson)){
+        console.log("installation des packages....");
+        exec(`npm install`,{projectRoot}).finally(()=>{
+            console.log("application "+appName+" créée.");
+        });
+    }
+    
+});
