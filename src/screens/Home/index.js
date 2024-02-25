19e909fa018ddc6a2c9761c8f5dc46b4d17a6ef1
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-02-25 11:18:43 +0100

    Modifier l'action déclencheur des paramètres sites internet de l'application

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,18 +3,12 @@ import { View, StyleSheet,TouchableWithoutFeedback } from 'react-native';
 import Browser,{session,EVENTS} from "$layouts/Browser";
 import {useState,useEffect} from "react";
 import { useApp } from '../../hooks';
-import APP from "$capp/instance";
-import {useRef} from "react";
 import {navigate} from "$cnavigation";
 import { screenName as siteScreenName } from '../Sites/utils';
-import AppBar from "$components/AppBar";
 
 export default function HomeScreen(props) {
   const [active,setActive] = useState(session.getActive());
   const {updateTheme} = useApp();
-  const pressCountRef = useRef(0);
-  const timeoutRef = useRef(null);
-  const timeRef = useRef(new Date().getTime());
   const cActive = session.getActive();
   useEffect(()=>{
     if(cActive?.name !== active?.name || cActive?.url !== active?.url){
@@ -22,18 +16,10 @@ export default function HomeScreen(props) {
     }
   },[JSON.stringify(cActive)]);
   return (<>
-    <TouchableWithoutFeedback style={styles.container} onPress={(e)=>{
-      if(new Date().getTime() - timeRef.current > 30000) {
-        pressCountRef.current = 0;
-        timeoutRef.current = new Date().getTime();
-        return;
-      }
-      pressCountRef.current++;
-      if(pressCountRef.current >= 5){
-        pressCountRef.current = 0;
-        timeoutRef.current = new Date().getTime();
+    <TouchableWithoutFeedback style={styles.container} 
+      delayLongPress = {2000}
+      onLongPress={(e)=>{
         return navigate(siteScreenName);
-      }
     }}>
       <Browser
         style = {[styles.browser1]}
